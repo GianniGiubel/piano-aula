1d69268aaa864771a530434b9c9776e0b1483b3b
Gianni <gianni.giubel@gmail.com>
2023-10-24 00:33:18 -0300

    finalizado projeto do piano virtual

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,11 +1,41 @@
 const pianoKeys = document.querySelectorAll('.key')
-let audio = new Audio("./tunes")
+const volume = document.querySelector('.volume-slider input')
+const keysCheck = document.querySelector('.keys-check input')
 
- const playTune = (key)=> {
+let audio = new Audio("src/tunes/a.wav")
 
- }
+const playTune = (key)=> {
+    audio.src = `src/tunes/${key}.wav`
+    audio.play()
+    
+    const activatedKey = document.querySelector(`[data-key="${key}"]`)    
+    activatedKey.classList.add("active")
+    setTimeout(()=> {
+        activatedKey.classList.remove("active")
+    },150)
+}
 
-pianoKeys.forEach((key)=> {
-    console.log(key.innerText)
-    key.addEventListener("click",()=> playTune)
+const mapedKeys = []
+
+pianoKeys.forEach((element)=> {    
+    element.addEventListener("click",()=> 
+    playTune(element.dataset.key))
+    mapedKeys.push(element.dataset.key)      
 })
+
+document.addEventListener("keydown",(event)=> {
+    if(mapedKeys.includes(event.key)) {
+        playTune(event.key)
+    }
+})
+
+const handleVolume = (event) => {
+    audio.volume = event.target.value
+}
+
+const showKeys = () => {
+    pianoKeys.forEach(element => element.classList.toggle("hide"))
+}
+
+volume.addEventListener("input", handleVolume)
+keysCheck.addEventListener("click", showKeys)
